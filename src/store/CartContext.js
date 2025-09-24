9fb85af12aa9a396ea761281e63a3b2aefafccbd
Will D <diweizhi@gmail.com>
2025-09-24 13:18:34 +1000

    Revert "couter"
    This reverts commit b4a072184490f3f87399b82847f80780fe9af466.

--- src/store/CartContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export const CartContext = createContext({
-  items: [],
-  totalPrice: 0,
-  totalAmount: 0,
-  addMeal: () => {},
-  removeMeal: () => {},
-});
