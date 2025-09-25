c7dc6fa2e9549426b08793a3d9ce2ab0e3777d38
Will D <diweizhi@gmail.com>
2025-09-25 11:49:37 +1000

    count again

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,7 +1,14 @@
+import Meal from './Meal/Meal';
 import classes from './Meals.module.css';
 
-const Meals = () => {
-  return <div className={classes.Meals}>Meals</div>;
+const Meals = ({ meals }) => {
+  return (
+    <div className={classes.Meals}>
+      {meals.map((meal) => (
+        <Meal key={meal.id} meal={meal} />
+      ))}
+    </div>
+  );
 };
 
 export default Meals;
