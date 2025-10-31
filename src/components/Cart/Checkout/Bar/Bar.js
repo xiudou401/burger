234ae88832a22be13ac703cc11caa3076160bb0d
Will D <diweizhi@gmail.com>
2025-10-31 12:34:43 +1100

    bar

--- src/components/Cart/Checkout/Bar/Bar.js
+++ src/components/Cart/Checkout/Bar/Bar.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import classes from './Bar.module.css';
 
-const Bar = () => {
-  return <div>Bar</div>;
+const Bar = ({ totalPrice }) => {
+  return (
+    <div className={classes.Bar}>
+      <div className={classes.TotalPrice}>{totalPrice}</div>
+      <button className={classes.Button}>Pay</button>
+    </div>
+  );
 };
 
 export default Bar;
