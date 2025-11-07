6386b5d940a613bf3bdf0e4435b5b76480fb2ba1
Will D <diweizhi@gmail.com>
2025-11-07 13:04:05 +1100

    dropback

---
+++ src/components/Cart/CartDetails/CartDetails.js
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../../../store/CartContext';
+import classes from './CartDetails.module.css';
+
+const CartDetails = () => {
+  const cartCtx = useContext(CartContext);
+  return <div></div>;
+};
+
+export default CartDetails;
---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import classes from './Backdrop.module.css';
+
+const backdropRoot = document.getElementById('backdrop-root');
+const Backdrop = (props) => {
+  return ReactDOM.createPortal(
+    <div {...props} className={`${classes.Backdrop} ${props.className}`}>
+      {props.children}
+    </div>,
+    backdropRoot
+  );
+};
+
+export default Backdrop;
