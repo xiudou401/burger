5b06b2e685aa4ede17d9d7e5999bbd4720515d70
Will D <diweizhi@gmail.com>
2025-11-10 12:08:43 +1100

    Checkout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,12 +3,14 @@ import { CartContext } from '../../store/CartContext';
 import classes from './Cart.module.css';
 import iconImg from '../../asset/bag.png';
 import CartDetails from './CartDetails/CartDetails';
+import Checkout from './Checkout/Checkout';
 
 const Cart = () => {
   const cartCtx = useContext(CartContext);
   const { totalQuantity, totalPrice } = cartCtx;
 
   const [cartDetails, setCartDetails] = useState(false);
+  const [showCheckout, setShowCheckout] = useState(false);
 
   const toggleCartDetails = () => {
     if (cartCtx.totalQuantity === 0) {
@@ -19,15 +21,24 @@ const Cart = () => {
     console.log(cartDetails);
   };
 
+  const showCheckoutHandler = () => {
+    setShowCheckout(true);
+  };
+  const hideCheckoutHandler = () => {
+    setShowCheckout(false);
+  };
+
   useEffect(() => {
     if (cartCtx.totalQuantity === 0) {
       setCartDetails(false);
+      setShowCheckout(false);
     }
   }, [cartCtx.totalQuantity]);
 
   return (
     <div className={classes.Cart} onClick={toggleCartDetails}>
       {cartDetails && <CartDetails />}
+      {showCheckout && <Checkout hideCheckoutHandler={hideCheckoutHandler} />}
       <div className={classes.CartIcon}>
         <img className={classes.CartIconImg} src={iconImg} alt="cart" />
         {totalQuantity === 0 ? null : (
@@ -43,6 +54,7 @@ const Cart = () => {
         className={`${classes.BuyButton} ${
           totalQuantity === 0 ? classes.Disabled : ''
         }`}
+        onClick={showCheckoutHandler}
       >
         Buy
       </button>
--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Checkout.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from 'react';
+import { CartContext } from '../../../store/CartContext';
+import CheckoutItem from './CheckoutItem/CheckoutItem';
 
-const Checkout = () => {
-  return <div></div>;
+const checkoutRoot = document.getElementById('checkout-root');
+
+const Checkout = ({ hideCheckoutHandler }) => {
+  const cartCtx = useContext(CartContext);
+  return ReactDOM.createPortal(
+    <div
+      className={classes.Checkout}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <div className={classes.Close}>
+        <FontAwesomeIcon icon={faXmark} onClick={hideCheckoutHandler} />
+      </div>
+      <div className={classes.MealDesc}>
+        <header className={classes.Header}>
+          <h2 className={classes.Title}>Order Details</h2>
+        </header>
+        <div>
+          {cartCtx.items.map((item) => (
+            <CheckoutItem key={item} meal={item} />
+          ))}
+        </div>
+        <footer className={classes.Footer}>
+          <p className={classes.TotalPrice}>{cartCtx.totalPrice}</p>
+        </footer>
+      </div>
+    </div>,
+    checkoutRoot
+  );
 };
 
 export default Checkout;
