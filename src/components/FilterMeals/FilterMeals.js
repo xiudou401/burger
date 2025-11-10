cafaf0769d61a96802ec8a54a93b5b9096000256
Will D <diweizhi@gmail.com>
2025-11-10 15:25:34 +1100

    filter timer

--- src/components/FilterMeals/FilterMeals.js
+++ src/components/FilterMeals/FilterMeals.js
@@ -5,6 +5,21 @@ import { useEffect, useRef, useState } from 'react';
 
 const FilterMeals = ({ filterMealsHandler }) => {
   const [keyword, setKeyword] = useState('');
+  const timer = useRef(null);
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    timer.current = setTimeout(() => {
+      filterMealsHandler(keyword);
+    }, 300);
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer);
+      }
+    };
+  }, [keyword, filterMealsHandler]);
 
   return (
     <div className={classes.FilterMeals}>
@@ -13,6 +28,7 @@ const FilterMeals = ({ filterMealsHandler }) => {
           type="text"
           className={classes.SearchInput}
           placeholder="Key in..."
+          onChange={handleChange}
         />
         <FontAwesomeIcon icon={faSearch} className={classes.SearchIcon} />
       </div>
