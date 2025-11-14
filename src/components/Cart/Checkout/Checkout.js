922ec3f60e0c2961e8b58fb64361eed42c806175
Will D <diweizhi@gmail.com>
2025-11-14 13:51:49 +1100

    bar

--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -5,6 +5,7 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 import { CartContext } from '../../../store/CartContext';
 import CheckoutItem from './CheckoutItem/CheckoutItem';
+import Bar from './Bar/Bar';
 const CheckoutRoot = document.getElementById('checkout-root');
 const Checkout = ({ hideShowCheckoutHandler }) => {
   const cartCtx = useContext(CartContext);
@@ -23,6 +24,7 @@ const Checkout = ({ hideShowCheckoutHandler }) => {
           ))}
         </div>
       </div>
+      <Bar totalPrice={cartCtx.totalPrice} />s
     </div>,
     CheckoutRoot
   );
