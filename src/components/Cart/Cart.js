90c2bd8c934e575260632a793e09b1e75efdccd4
Will D <diweizhi@gmail.com>
2025-12-11 14:55:35 +1100

    cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import classes from './Cart.module.css';
+import iconImg from '../../asset/bag.png';
+import { useContext, useState } from 'react';
+import { CartContext } from '../../store/CartContext';
+import CartDetails from './CartDetails/CartDetails';
+
+const Cart = () => {
+  const cartCtx = useContext(CartContext);
+  const [showDetails, setShowDetails] = useState(false);
+  const toggleCartDetails = () => {
+    if (cartCtx.totalQuantity === 0) {
+      setShowDetails(false);
+      return;
+    }
+    setShowDetails((prevState) => !prevState);
+  };
+
+  return (
+    <div className={classes.Cart} onClick={toggleCartDetails}>
+      {showDetails && <CartDetails />}
+      <div className={classes.CartIcon}>
+        <img className={classes.CartIconImg} src={iconImg} alt="Shopping bag" />
+        {cartCtx.totalQuantity && (
+          <span className={classes.TotalQuantity}>{cartCtx.totalQuantity}</span>
+        )}
+      </div>
+      {cartCtx.totalQuantity === 0 ? (
+        <p className={classes.NoMeal}>Cart is empty</p>
+      ) : (
+        <p className={classes.Price}>{cartCtx.totalPrice}</p>
+      )}
+      <button
+        className={`${classes.BuyButton} ${
+          cartCtx.totalQuantity === 0 ? classes.Disabled : ''
+        }`}
+      >
+        Buy
+      </button>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/components/Cart/CartDetails/CartDetails.js
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import classes from './CartDetails.module.css';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useContext } from 'react';
+import { CartContext } from '../../../store/CartContext';
+import MealItem from '../../Meals/Meal/MealItem';
+
+const CartDetails = () => {
+  const cartCtx = useContext(CartContext);
+  return (
+    <Backdrop>
+      <div className={classes.CartDetails}>
+        <header className={classes.Header}>
+          <h2 className={classes.Title}>餐品详情</h2>
+          <div className={classes.Clear}>
+            <FontAwesomeIcon icon={faTrash} />
+            <span>清空购物车</span>
+          </div>
+        </header>
+        <div className={classes.MealList}>
+          {cartCtx.items.map((item) => (
+            <MealItem key={item.id} meal={item} />
+          ))}
+        </div>
+      </div>
+    </Backdrop>
+  );
+};
+
+export default CartDetails;
