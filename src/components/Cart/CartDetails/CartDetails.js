a308bc9f125adc4c9725eec3271d3e886606f207
Will D <diweizhi@gmail.com>
2025-12-11 18:43:39 +1100

    Confirm

--- src/components/Cart/CartDetails/CartDetails.js
+++ src/components/Cart/CartDetails/CartDetails.js
@@ -2,12 +2,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './CartDetails.module.css';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../../../store/CartContext';
 import MealItem from '../../Meals/Meal/MealItem';
+import Confirm from '../../UI/Confirm/Confirm';
 
 const CartDetails = () => {
   const cartCtx = useContext(CartContext);
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const onCancel = () => {
+    setShowConfirm(false);
+  };
+  const onOk = () => {
+    cartCtx.cartDispatch({ type: 'CLEAR' });
+  };
   return (
     <Backdrop>
       <div
@@ -16,12 +25,19 @@ const CartDetails = () => {
           e.stopPropagation();
         }}
       >
+        {showConfirm && (
+          <Confirm
+            confirmText="Are you sure?"
+            onCancel={onCancel}
+            onOk={onOk}
+          />
+        )}
         <header className={classes.Header}>
           <h2 className={classes.Title}>餐品详情</h2>
           <div
             className={classes.Clear}
             onClick={() => {
-              cartCtx.cartDispatch({ type: 'CLEAR' });
+              setShowConfirm(true);
             }}
           >
             <FontAwesomeIcon icon={faTrash} />
---
+++ src/components/UI/Confirm/Confirm.js
@@ -0,0 +1,22 @@
+import Backdrop from '../Backdrop/Backdrop';
+import classes from './Confirm.module.css';
+
+const Confirm = ({ confirmText, onCancel, onOk }) => {
+  return (
+    <Backdrop className={classes.ConfirmOuter}>
+      <div className={classes.Confirm}>
+        <p className={classes.ConfirmText}>{confirmText}</p>
+        <div>
+          <button className={classes.Cancel} onClick={onCancel}>
+            Cancel
+          </button>
+          <button className={classes.Ok} onClick={onOk}>
+            Ok
+          </button>
+        </div>
+      </div>
+    </Backdrop>
+  );
+};
+
+export default Confirm;
