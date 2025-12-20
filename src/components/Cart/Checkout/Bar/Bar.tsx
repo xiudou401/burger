4d627b84abda51371ad9fc52704e039b1ed323e0
Will D <diweizhi@gmail.com>
2025-12-20 15:47:09 +1100

    bar

--- src/components/Cart/Checkout/Bar/Bar.tsx
+++ src/components/Cart/Checkout/Bar/Bar.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import classes from './Bar.module.css';
 
 interface BarProps {
-  totalPrice: Number;
+  totalPrice: number;
 }
 
 const Bar = ({ totalPrice }: BarProps) => {
-  return <div></div>;
+  return (
+    <div className={classes.Bar}>
+      <div className={classes.TotalPrice}>{totalPrice}</div>
+      <button className={classes.Button}>Pay</button>
+    </div>
+  );
 };
 
 export default Bar;
