9287fa64a3baae8ed9af43b2a50f79197f0f9925
Will D <diweizhi@gmail.com>
2026-06-09 06:10:10 +1000

    Simplify cart persistence dependencies

--- frontend/src/store/cart/hooks/useCartPersistence.ts
+++ frontend/src/store/cart/hooks/useCartPersistence.ts
@@ -1,11 +1,8 @@
 import { useEffect } from 'react';
 import type { CartStoredItem } from '../../../types/cart';
-import { cartSignature } from '../utils/cart-signature';
 
 export const useCartPersistence = (items: CartStoredItem[]) => {
-  const itemsSignature = cartSignature(items);
-
   useEffect(() => {
     localStorage.setItem('CartItemsState', JSON.stringify(items));
-  }, [items, itemsSignature]);
+  }, [items]);
 };
